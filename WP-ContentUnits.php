b0c89110e61a3fc226fdf159ed5b09a366a4c017
Frederik Rabøl <frederik-rm@hotmail.com>
2016-10-27 09:10:39 +0200

    updated version number

--- WP-ContentUnits.php
+++ WP-ContentUnits.php
@@ -4,7 +4,7 @@ Plugin Name: WordPress Manual Content Units v2
 Plugin URI: https://github.com/BenjaminMedia/WP-ContentUnits
 Description: Plugin that allows you to manually add Specific Content Units to WordPress
 Author: Frederik Rabøl & Alf Henderson
-Version: 2.1.9.5
+Version: 2.3
 Author URI: http://www.bonnierpublications.com
 */
 
